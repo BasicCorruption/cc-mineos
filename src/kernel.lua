cbd4a71d581aad1861c921466608398c98348c8c
BasicCorruption <saytleader@outlook.com>
2022-09-02 07:22:06 -0400

    Update kernel.lua

--- src/kernel.lua
+++ src/kernel.lua
@@ -1,3 +1,10 @@
 term.clear()
-term.setCursorPos(1, 1)
-term.write("test")
+term.setCursorPos(1,1)
+
+local wcy = 1
+
+local function println(text)
+    term.setCursorPos(1,wcy)
+    term.write(text);
+    wcy = wcy + 1
+end
